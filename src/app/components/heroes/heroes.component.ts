406b09ffdb30f37014ebea5a220a842272f060a8
uriart <loiro_8@hotmail.com>
2020-09-14 21:26:19 +0200

    Primer commit a git

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: [
+  ]
+})
+export class HeroesComponent implements OnInit {
+
+heroes:Heroe[] = [];
+
+  constructor( private _heroesService:HeroesService ) {
+
+   }
+
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
+  }
+
+}

16a3d20598aff8230170e9efacbe270ac028f0b5
uriart <loiro_8@hotmail.com>
2020-09-28 21:29:50 +0200

    Otra prueba

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
+app.use(express.static(__dirname + '/src'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // default Heroku port
